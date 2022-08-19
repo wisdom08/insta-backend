d5acf54a5c679dcd342f583103b4370ce20767cb
dae-woo <123kdw@naver.com>
2022-08-19 21:05:01 +0900

    #4 UserService 추가  - userintro입력해도 null값 처리됨  - 패스워드 암호화 미구현

---
+++ src/main/java/com/insta/service/UserService.java
@@ -0,0 +1,53 @@
+package com.insta.service;
+
+
+import com.insta.auth.SignupRequestDto;
+import com.insta.model.User;
+import com.insta.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public String registerUser(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+        String passwordCheck = requestDto.getPasswordCheck();
+        String userintro = requestDto.getUserintro();
+
+        Optional<User> userTocheck = this.userRepository.findByUsername(username);
+
+        if(userTocheck.isPresent()){
+            return "중복된 아이디 입니다.";
+        }
+        else if(!Objects.equals(password, passwordCheck)){
+            return "비밀번호가 동일하지 않습니다.";
+        }
+        else if(Objects.equals(username, "")){
+            return "아이디를 입력해주세요.";
+        }
+        else if(Objects.equals(password, "")) {
+            return "비밀번호를 입력해주세요.";
+        }
+        else {
+            requestDto.setPassword(password);
+            User user = new User(username, password);
+            this.userRepository.save(user);
+            return "회원가입에 성공하였습니다.";
+        }
+
+    }
+
+
+
+
+
+}
