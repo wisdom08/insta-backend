4505400b16b2b417e6ed48dd64789e534ee1a107
dae-woo <123kdw@naver.com>
2022-08-20 18:15:57 +0900

    # 로그인 기능, jwt토큰 시큐리티 기능 추가 수정  - 파일 재업로드

---
+++ src/main/java/com/insta/auth/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.insta.auth;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
