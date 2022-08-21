3dc2ff24d4998c6a2eab491eea43bbaa07f1f6bf
wisdom08 <wisdom08@kakao.com>
2022-08-21 22:43:35 +0900

    feat: #9 게시글 조회할 때 댓글 개수 응답바디에 추가
    - 연관관계 매핑
    - 게시글 응답 DTO 에 댓글 개수 추가

--- src/main/java/com/insta/dto/article/ArticleResponseDto.java
+++ src/main/java/com/insta/dto/article/ArticleResponseDto.java
@@ -6,14 +6,16 @@ import lombok.Getter;
 @Getter
 public class ArticleResponseDto {
     private Long id;
+    private int numberOfComments;
 
     protected ArticleResponseDto() {}
 
-    private ArticleResponseDto(Long id) {
+    private ArticleResponseDto(Long id, int numberOfComments) {
         this.id = id;
+        this.numberOfComments = numberOfComments;
     }
 
     public static ArticleResponseDto from(Article entity) {
-      return new ArticleResponseDto(entity.getId());
+      return new ArticleResponseDto(entity.getId(), entity.getComments().size());
     }
 }
