8fcfee8818fa3637302230e07a68a59c1830ad96
wisdom08 <wisdom08@kakao.com>
2022-08-22 20:42:05 +0900

    feat: #21 게시글 전체 조회 시, 슬라이싱 처리

--- src/main/java/com/insta/repository/ArticleRepo.java
+++ src/main/java/com/insta/repository/ArticleRepo.java
@@ -1,7 +1,17 @@
 package com.insta.repository;
 
 import com.insta.model.Article;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepo extends JpaRepository<Article, Long>{
+
+    @Query("select a from Article a " +
+            "where a.id < :id " +
+            "order by a.id desc")
+    Slice<Article> findAllOrderByIdDesc(@Param("id") Long id, Pageable pageable);
+
 }
