abecf49c4f4e501015b48c67026eeb6242665628
wisdom08 <wisdom08@kakao.com>
2022-08-25 18:19:16 +0900

    fix: 인코딩 방식 변경
    - 필터에서 스프링 설정 추가 방식으로 변경

--- src/main/java/com/insta/InstaBackendApplication.java
+++ src/main/java/com/insta/InstaBackendApplication.java
@@ -2,10 +2,8 @@ package com.insta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,13 +12,5 @@ public class InstaBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InstaBackendApplication.class, args);
-    }
-
-    @Bean
-    public CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
     }
 }
