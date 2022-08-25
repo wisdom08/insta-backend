78f77922c253eb640757a2426fac5e3f4d148757
wisdom08 <wisdom08@kakao.com>
2022-08-25 18:11:29 +0900

    test: 인코딩 필터 테스트

--- src/main/java/com/insta/InstaBackendApplication.java
+++ src/main/java/com/insta/InstaBackendApplication.java
@@ -2,8 +2,10 @@ package com.insta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,5 +14,13 @@ public class InstaBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InstaBackendApplication.class, args);
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
     }
 }
