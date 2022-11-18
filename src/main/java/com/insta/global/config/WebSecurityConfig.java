33be68b9c54ebacfb3ea007212b71c1b00e384bb
wisdom08 <wisdom08@kakao.com>
2022-11-18 21:33:19 +0900

    fix: url 경로 와일드카드 처리 추가

--- src/main/java/com/insta/global/config/WebSecurityConfig.java
+++ src/main/java/com/insta/global/config/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig {
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .csrf().ignoringAntMatchers("/api/articles", "/api/auth")
+                .csrf().ignoringAntMatchers("/api/articles/**", "/api/auth/**")
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and().headers().frameOptions().disable()
